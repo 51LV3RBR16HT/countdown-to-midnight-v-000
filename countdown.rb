3b21e475d6838dc43c4b6b50b8140768075b889f
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-03 04:40:36 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,8 @@
 #write your code here
 
-def countdown(x)
-  while x is <= 10
-  puts "#{x} SECOND(S)!"
+def countdown(timer)
+  while timer is > 0
+  puts "#{timer} SECOND(S)!"
   #   x -= 1
   #   # return "HAPPY NEW YEAR!"
 
